6674c99adbc4d40d8ad52a2262f26ac105065753
vjosso <vjosso@etu.u-bordeaux.fr>
2021-09-06 10:07:04 +0200

    premiere sceance

---
+++ projetReprise/2021ProjetLabyJossoVictor/src/main/java/labyrinthe/Salle.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labyrinthe;
+
+/**
+ *
+ * @author vjosso
+ */
+public class Salle implements ISalle {
+    
+    protected int x;
+    protected int y;
+    
+    public Salle(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    @Override
+    public int getX(){
+        return 0;
+    }
+    
+    @Override
+    public int getY(){
+        return 0;
+    }
+    
+    @Override
+    public boolean estAdjacente(ISalle autre){
+        return false;
+    }
+    
+}
