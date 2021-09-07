4d64a4b6634b32d25c291f71624df90aff804a9e
vjosso <vjosso@etu.u-bordeaux.fr>
2021-09-07 09:28:47 +0200

    deux premier test fini mais fichier introuvable

--- projetReprise/2021ProjetLabyJossoVictor/src/test/java/TestFichiersLabyrinthe.java
+++ projetReprise/2021ProjetLabyJossoVictor/src/test/java/TestFichiersLabyrinthe.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 import java.io.File;
+import labyrinthe.Salle;
 import org.junit.Test;
 import outils.Fichier;
 
@@ -25,12 +26,20 @@ public class TestFichiersLabyrinthe {
         String nomFichier = f.getName();
         Fichier fichier = new Fichier(nomFichier);
         
-        if(!nomFichier.endsWith("txt"))return false;
+        if(!nomFichier.endsWith(".txt")) return false;
         
+        //try catch inutile car aucun moyen d'avoir un retour de lettre, retour par défaut -1 
+        //trouver un moyen de faire la différence entre retour dû à une erreur et dû à la fin du fichier
         try{
+            int l;
+            int h;
             int cpt = 0;
             while(cpt != -1){
                 cpt = fichier.lireNombre();
+                if(cpt == -1) break;
+                l = cpt;
+                h = fichier.lireNombre();
+                if(l < 0 || h < 0) return false;
             }
         } catch (Exception e){ //Exception à finir et rajouter
             System.err.println(e);
@@ -45,13 +54,26 @@ public class TestFichiersLabyrinthe {
         File repertoire = new File("labys/");
         File[] fichiers = getFiles(repertoire);
         
-        for(File f : fichiers){
-            if(testCoordonneesSallesFichier(f)){
-                System.out.println("test du fichier" + f.toString());
+        Fichier f;
+        
+        
+        for(File file : fichiers){
+            if(testCoordonneesSallesFichier(file)){
+                System.out.println("test du fichier" + file.toString());
                 
+                f = new Fichier(file.toString());
+                
+                int longueur = f.lireNombre();
+                int hauteur = f.lireNombre();
+                
+                Salle entree = new Salle(f.lireNombre(), f.lireNombre());
+                Salle sortie = new Salle(f.lireNombre(), f.lireNombre());
+                
+                assertTrue(entree.getX() > 0 && longueur >= entree.getX() && sortie.getX() > 0 && longueur >= sortie.getX());
+                assertTrue(entree.getY() > 0 && hauteur >= entree.getY() && sortie.getY() > 0 && hauteur >= sortie.getY());
             }
             else{
-                System.out.println("fichier " + f.toString() + " invalide");
+                System.out.println("fichier " + file.toString() + " invalide");
             }
         }
     }
